64823e39e21d473874a1174cf643342bc928d79a
Zhu Zhanyan <program.nom@gmail.com>
2021-02-07 23:31:51 +0800

    chore: Initial Commit: Setup project and .gitignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace np_se_assign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
