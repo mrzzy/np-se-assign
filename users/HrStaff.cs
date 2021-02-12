2a79414b0476473a48f9f029fce73ce970caf2ed
Zhu Zhanyan <program.nom@gmail.com>
2021-02-13 01:12:17 +0800

    chore: Format HrStaff.cs

--- users/HrStaff.cs
+++ users/HrStaff.cs
@@ -1,19 +1,18 @@
 using System;
 
-namespace NP.SE.Assignment 
+namespace NP.SE.Assignment
 {
     /* Represents a HR Staff that administers the parking system */
     public class HrStaff: User
     {
-        public HrStaff(string userId, string name, string password, string mobileNumber, string paymentMode) 
+        public HrStaff(string userId, string name, string password, string mobileNumber, string paymentMode)
         {
             this.UserId = userId;
             this.Name = name;
             this.PasswordHash = Hash(password);
             this.MobileNumber = mobileNumber;
-            
         }
-            
+
         public void GenerateFinancialReport()
         {
             throw new NotImplementedException();
