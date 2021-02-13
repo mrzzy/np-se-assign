cf5a8e8b39ff129da12b76551c77be4d4f9c10ea
Ana <anakatrina1101@gmail.com>
2021-02-14 00:36:52 +0800

    Fixed spelling error

--- Program.cs
+++ Program.cs
@@ -66,7 +66,7 @@ namespace NP.SE.Assignment
 
                     if (currentUser is null)
                     {
-                        Console.WriteLine("Login unsuccesful!");
+                        Console.WriteLine("Login unsuccessful!");
                         Console.WriteLine("");
                     }
 
