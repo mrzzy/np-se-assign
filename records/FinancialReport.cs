37306dd7725ae0ec8bacdb81ac24d16a45a01f90
cheskalazo <57380689+cheskalazo@users.noreply.github.com>
2021-02-13 23:33:36 +0800

    added the necessary inputs

--- records/FinancialReport.cs
+++ records/FinancialReport.cs
@@ -2,15 +2,59 @@ using System;
 
 namespace NP.SE.Assignment
 {
-    public List<ParkingRecord> records;
-    public string carparkChoice; 
-    public DateTime month; 
+   
     
     public class FinancialReport
     {
+        public List<ParkingRecord> records;
+        public int month; 
         public string GenerateReport()
         {
-            //Console.WriteLine("Financial Report for" + month + "in" + carparkChoice + "."); 
+            Console.Write("Enter the month for the Financial Report:"); 
+            int monthchoice = Convert.ToInt32(Console.ReadLine()); 
+            
+            Console.Write("Do you want to generate reports for all Carparks? [Y/N]"); 
+            string carparkchoice = Console.ReadLine(); 
+            
+            if (carparkchoice == "N")
+            {
+                Console.Write("Choose a carpark location." + \n + "Carpark 1" + \n + "Carpark 2" + \n + "Carpark 3" + \n + "Enter nuumber of Carpark:");
+                int locationchoice = Convert.ToInt32(Console.ReadLine()); 
+                
+                Console.Write("Choose a vehicle type." + \n + "1. Car" + \n + "2. Motorcyle" + \n + "Enter choice:");
+                int vtypechoice = Convert.ToInt32(Console.ReadLine()); 
+                
+                if (vtypechoice == 1)
+                {
+                    /// enter the report details and codes here
+                }
+                
+                else (vtypechoice == 2)
+                {
+                    /// enter the report details and codes here
+                }
+                
+            }
+            
+            elseif(carparkchoice == "Y"); 
+            {
+                Console.Write("Choose a vehicle type." + \n + "1. Car" + \n + "2. Motorcyle" + \n + "Enter choice:");
+                int vtypechoice = Convert.ToInt32(Console.ReadLine()); 
+                
+                if (vtypechoice == 1)
+                {
+                    /// enter the report details and codes here
+                }
+                
+                else (vtypechoice == 2)
+                {
+                    /// enter the report details and codes here
+                }
+                
+            }
+                
+                    
+            
         }
     }
 }
