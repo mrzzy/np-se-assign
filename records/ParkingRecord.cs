e0cac4ed360cbedc8f0ffe5d06007279aba8b88e
Ana <anakatrina1101@gmail.com>
2021-02-14 10:36:42 +0800

    Feat: carpark park and exit

--- records/ParkingRecord.cs
+++ records/ParkingRecord.cs
@@ -10,7 +10,7 @@ namespace NP.SE.Assignment
         public Carpark Carpark {get; private set; }
         public DateTime EntryTime  { get; private set; }
         public DateTime? ExitTime { get; private set; }
-        public double AmountCharged { get; private set; }
+        public double AmountCharged { get; set; }
 
         public ParkingRecord(int id, Vehicle vehicle, Carpark carpark, DateTime entryTime)
         {
@@ -20,6 +20,17 @@ namespace NP.SE.Assignment
             EntryTime = entryTime;
             ExitTime = null;
             AmountCharged = 0.0;
+        }
+
+        public void setExitTime(DateTime endTime)
+        {
+            if (ExitTime == null)
+                ExitTime = endTime;
+        }
+
+        public void setAmtCharged(double amt)
+        {
+            AmountCharged = amt;
         }
     }
 }
