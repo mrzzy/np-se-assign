68a083c9a4f6fb44ac4f99873ad5c6c2c8c78230
0 Lazo Francheska Jullien Christ Pido <s10185804@connect.np.edu.sg>
2021-02-14 21:17:15 +0800

    financial repory

--- records/FinancialReport.cs
+++ records/FinancialReport.cs
@@ -15,10 +15,14 @@ namespace NP.SE.Assignment
         {
             Console.Write("Enter the month for the Financial Report:"); 
             int monthchoice = Convert.ToInt32(Console.ReadLine()); 
+
             
             Console.Write("Do you want to generate reports for all Carparks? [Y/N]"); 
             string carparkchoice = Console.ReadLine(); 
-            
+            int countCars = 0;
+            int countMotors = 0;
+            int totalRevenue = 0; 
+           
             if (carparkchoice == "N")
             {
                 Console.Write("Choose a carpark location.\nCarpark 1\nCarpark 2\nCarpark 3\nEnter nuumber of Carpark:");
@@ -26,17 +30,35 @@ namespace NP.SE.Assignment
                 
                 Console.Write("Choose a vehicle type.\n1. Car\n2. Motorcyle\nEnter choice:");
                 int vtypechoice = Convert.ToInt32(Console.ReadLine()); 
-                
+                VehicleType vehicleType; 
+
                 if (vtypechoice == 1)
                 {
-                    /// enter the report details and codes here
+                    //Console.Write("=============" + DateTime.Now + "============="); 
+                    //Console.Write("Financial Report of {} in the month of {}", vtypechoice, monthchoice);
+                    //Console.Write("");
+                    //Console.Write("Carpark {}", carparkchoice);
+                    vehicleType = VehicleType.Car;
                 }
+                
                 
                 else if (vtypechoice == 2)
                 {
-                    /// enter the report details and codes here
+                    vehicleType = VehicleType.Motorcycle; 
                 }
+
+                foreach (ParkingRecord record in ParkingRecordList) {
+                    //int countCars = 0;
+                    //int countMotors = 0; 
+                    record.EntryTime.Month; 
+                    if record.EntryTime.Month != monthchoice {continue}
+                    if (vehicleType = VehicleType.Car) {countCars += 1}
+                    else (vehicleType = VehicleType.Motorcycle) {countMotors +=1}
+
+                    totalRevenue += record.AmountCharged; 
+            }
                 
+
             }
             
             else if(carparkchoice == "Y")
@@ -46,17 +68,40 @@ namespace NP.SE.Assignment
                 
                 if (vtypechoice == 1)
                 {
-                    /// enter the report details and codes here
+                    //Console.Write("=============" + DateTime.Now + "============="); 
+                    //Console.Write("Financial Report of {} in the month of {}", vtypechoice, monthchoice);
+                    //Console.Write("");
+                    //Console.Write("Carpark {}", carparkchoice);
+                    vehicleType = VehicleType.Car;
                 }
+                
                 
                 else if (vtypechoice == 2)
                 {
-                    /// enter the report details and codes here
+                    vehicleType = VehicleType.Motorcycle; 
                 }
+
+                foreach (ParkingRecord record in ParkingRecordList) {
+                    record.EntryTime.Month; 
+                    if record.EntryTime.Month != monthchoice {continue}
+                    if (vehicleType = VehicleType.Car) {countCars += 1}
+                    else (vehicleType = VehicleType.Motorcycle) {countMotors +=1}
+
                 
-            }
+                }
 
             // return something to prevent error 
+            Console.Write("=============" + DateTime.Now + "============="); 
+            Console.Write("Financial Report of {} in the month of {}", vtypechoice, monthchoice);
+            Console.Write("");
+            Console.Write("Carpark {}", carparkchoice);
+            Console.Write("Number of Entries: {}", countCars);
+            Console.Write("Total Revenue Generated: {}", totalRevenue);
+
+
+
+            //Console.Write("Number of Season Pass Holders: {}",)
+
             return "";
         }
 
